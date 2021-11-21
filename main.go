fa803343104b294580141164d309c1fac40cc6e9
agajdosi <andreas.gajdosik@gmail.com>
2021-11-21 10:35:59 +0100

    Sketch the UI

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -14,36 +13,122 @@ func main() {
 	application := app.New()
 	window := application.NewWindow("Nomin")
 
-	curators := []string{
-		"Michal Novotný <michal.novotny@futura.cz>",
-		"Michal Novotný 2 <michal.novotny2@futura.cz>",
-	}
+	from := widget.NewSelectEntry(curators)
+	from.SetPlaceHolder("FROM:")
+	to := widget.NewSelectEntry(galleries)
+	to.SetPlaceHolder("TO:")
+	subject := widget.NewEntry()
+	subject.SetPlaceHolder("SUBJECT:")
+	text := widget.NewMultiLineEntry()
+	text.SetPlaceHolder("TEXT:")
 
-	galleries := []string{
-		"Michal Novotný <michal.novotny@futura.cz>",
-		"Michal Novotný 2 <michal.novotny2@futura.cz>",
-	}
+	server := widget.NewEntry()
+	server.SetPlaceHolder("SMTP server address")
+	port := widget.NewEntry()
+	port.SetPlaceHolder("Port number")
 
-	sender := widget.NewSelectEntry(curators)
-	recipient := widget.NewSelectEntry(galleries)
+	provider := widget.NewSelect(providers, func(option string) {
+		setProvider(option, server, port)
+	})
+	provider.PlaceHolder = "Choose your internet provider"
 
-	hello := widget.NewLabel("Hello Fyne!")
+	sendButton := widget.NewButton("Send Mail", func() {
+		verifyAndSend(from, to, subject, text, server, port)
+	})
 
-	window.SetContent(container.NewVBox(
-		hello,
-		sender,
-		recipient,
-		widget.NewButton("Send Mail", func() {
+	mailAbout := widget.NewRichTextWithText("Get recommended by a famous curator. Write an email recommending your artistic persona, select the sending curator and receiving gallery or other institution and just get recommended without any need for boring and slow networking!")
+	mailAbout.Wrapping = fyne.TextWrapWord
 
-			sendMail()
-		}),
-	))
+	smtpAbout := widget.NewRichTextWithText("To send the email Nomin needs the ADDRESS and the PORT of open SMTP mail server. You can insert one manually or choose SMTP server of your internet provider.")
+	smtpAbout.Wrapping = fyne.TextWrapWord
+
+	sendAbout := widget.NewRichTextWithText("Check the texts, addresses and configuration. If everything is allright, just send it out!")
+	sendAbout.Wrapping = fyne.TextWrapWord
+
+	smtpLayout := container.NewVBox(
+		smtpAbout,
+		provider,
+		server,
+		port,
+	)
+
+	mailLayout := container.NewVBox(
+		mailAbout,
+		from,
+		to,
+		subject,
+		text,
+	)
+
+	sendLayout := container.NewVBox(
+		sendAbout,
+		sendButton,
+	)
+
+	tabs := container.NewAppTabs(
+		container.NewTabItem("1 Write", mailLayout),
+		container.NewTabItem("2 Configure", smtpLayout),
+		container.NewTabItem("3 Send", sendLayout),
+	)
+
+	/*window.SetContent(container.NewVBox(
+		mailLayout,
+		smtpLayout,
+		sendLayout,
+	))*/
+
+	window.SetContent(tabs)
 
 	window.ShowAndRun()
+
 }
 
-func sendMail() {
-	fmt.Println("blabla")
-	//(from string, to string, subject string, message string, serverAddress string, serverPort string)
-	sender.SendMail("janzalesak@ffa.vutbr.cz", "andreas.gajdosik@gmail.com", "Hello!", "Hello Andreas, it is nice to chat again!", "smtp.vodafonemail.cz", "25")
+func verifyAndSend(from, to *widget.SelectEntry, subject, text, address, port *widget.Entry) {
+	sender.SendMail(
+		from.Text,
+		to.Text,
+		subject.Text,
+		text.Text,
+		address.Text,
+		port.Text,
+	)
+}
+
+var providers = []string{
+	"cz: UPC",
+	"cz: Vodafone",
+	"cz: T-Mobile",
+	"custom",
+}
+
+func setProvider(provider string, server, port *widget.Entry) {
+	var s, p string
+	switch provider {
+	case "cz: UPC":
+		s = "mail.upcmail.cz"
+		p = "25"
+	case "cz: Vodafone":
+		s = "smtp.vodafonemail.cz"
+		p = "25"
+	case "cz: T-Mobile":
+		s = "smtp.t-email.cz"
+		p = "25"
+	default:
+		s = ""
+		p = "25"
+	}
+
+	server.SetText(s)
+	port.SetText(p)
+
+}
+
+var curators = []string{
+	"Michal Novotný <michal.novotny@futura.cz>",
+	"Michal Novotný 2 <michal.novotny2@futura.cz>",
+}
+
+var galleries = []string{
+	"Michal Novotný <michal.novotny@futura.cz>",
+	"Michal Novotný 2 <michal.novotny2@futura.cz>",
 }
